0b58b1a15dfd93a73d5a0871fef72b72c250f56e
Jacob Weaver <jweaver60@gmail.com>
2014-06-15 16:57:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Definiv Dotfiles
+
+This is the beginning of my dotfiles repo. More information on how to use will come eventually.
---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Install all the basics, only need this one the first time I think
+
+echo "Installing Oh My ZSH"
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | ZSH=~/.dotfiles/zsh sh
+
